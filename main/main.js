54eb403bbc04a81b36f7b4993e8e49724d4deb26
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-10 09:05:44 -0800

    add variable-basic

---
+++ main/main.js
@@ -0,0 +1,24 @@
+var busavAilability="no"
+var moreHourwork=true;
+var wakeupTime=9;
+var anyFunctionForAnyone=true;
+var bodyTemp=100;
+var goingTotour=true;
+var ismanasuGood=true;
+var bikeWorking=true;
+var localMatch=true;
+var tempFestival=true;
+var israining=true;
+var worthyTopic="good";
+var movieRealse=true;
+
+if((busavAilability=="no")&&(moreHourwork==true)&&(wakeupTime>10)&&(anyFunctionForAnyone==true)&&(bodyTamp>=100)&&(goingToTour==true)&&(ismanasuGood!==true)&&(bikeWorking!=true)&&(tempFestival==true)&&(isRaining==true)||(worthyTopic!="good")&&(movieRelase==true))
+{       
+	console.log("Take leave");
+}
+                    
+else
+{
+	console.log("not leave");
+}
+
